dec7f89127099a3222538ec1897d82ae5bd3e311
Derek <chenjunqi07@icloud.com>
2020-08-03 16:11:59 +0800

    Initial Commit

---
+++ ToDo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Derek Chan on 2020/8/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
